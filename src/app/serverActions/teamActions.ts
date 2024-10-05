013375d1248918ba91d167826641527d46757d97
Mateusz Bodziak <matbodzi@gmail.com>
2024-10-05 22:29:48 +0200

    lint

--- src/app/serverActions/teamActions.ts
+++ src/app/serverActions/teamActions.ts
@@ -1,8 +1,7 @@
 "use server"
 
-
 export async function createRoomAction(param: FormData) {
-    const teamName = param.get("teamName");
+    const teamName = param.get("teamName") as string;
     const password = param.get("password");
 
     console.log(`Trying to acces a team named: ${teamName}`)
