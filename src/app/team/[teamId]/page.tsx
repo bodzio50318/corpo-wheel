e4507d1638510b75a5222a22b09d779456cea9ff
Mateusz Bodziak <matbodzi@gmail.com>
2024-10-06 14:07:47 +0200

    working wheel

--- src/app/team/[teamId]/page.tsx
+++ src/app/team/[teamId]/page.tsx
@@ -3,7 +3,12 @@ const items = [
     { id: '0', name: 'Item 1', chance: 10 },
     { id: '1', name: 'Item 2', chance: 20 },
     { id: '2', name: 'Item 3', chance: 30 },
-    { id: '3', name: 'Item 4', chance: 40 }
+    { id: '3', name: 'Item 4', chance: 30 },
+    { id: '4', name: 'Item 5', chance: 25 },
+    { id: '5', name: 'Item 6', chance: 25 },
+    { id: '6', name: 'Item 7', chance: 25 },
+    { id: '7', name: 'Item 8', chance: 25 },
+    { id: '8', name: 'Item 9', chance: 25 },
 ]
 
 
